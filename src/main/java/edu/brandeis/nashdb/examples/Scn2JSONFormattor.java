88dd5ce162b169e38caf9d72718555fb8646fea2
G <gkao123@brandeis.edu>
2018-11-24 18:05:17 -0500

    updated api endpoint response to incorporate Ryan's additions

--- src/main/java/edu/brandeis/nashdb/examples/Scn2JSONFormattor.java
+++ src/main/java/edu/brandeis/nashdb/examples/Scn2JSONFormattor.java
@@ -1,8 +1,15 @@
 package edu.brandeis.nashdb.examples;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -11,15 +18,24 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+import com.eclipsesource.json.WriterConfig;
 
+import edu.brandeis.nashdb.Fragment;
 import edu.brandeis.nashdb.FragmentationStrategy;
 import edu.brandeis.nashdb.NashDB;
 import edu.brandeis.nashdb.economic.cloud.Query;
+import edu.brandeis.nashdb.util.RenderExample;
+import info.rmarcus.JsonArrayCollector;
 
 public class Scn2JSONFormattor {
 	
 	private static final int NUM_TUPLES = 100;
 	private static final int NUM_FRAGMENTS = 5;
+	private static final int TIMESTAMPS_PER_TRANSITION = 10;
 	private static final Random r = new Random(43);
 	private static final Query[] queries = generateQueries(6);
 	private static int MAX_FRAGS_IN_VM = 0;
@@ -70,39 +86,62 @@ public class Scn2JSONFormattor {
 		return createJSON(nash);
 	}
 	public static String createJSON(NashDB nash) {
-		String stringToParse = nash.toJSON(FragmentationStrategy.OPTIMAL, NUM_FRAGMENTS);
-		JSONParser parser = new JSONParser();
-		JSONObject json = new JSONObject();
+		JsonArray output = new JsonArray();
 		
-		try {
-			json = (JSONObject) parser.parse(stringToParse);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			System.out.println("cannot parse");
-		}
-		JSONArray seriesArray = new JSONArray();
-		JSONArray fragments_fromJSON =  (JSONArray) json.get("fragments");
-		JSONArray vms_fromJSON = (JSONArray) json.get("vms");
-		JSONArray graphData_fromJSON = (JSONArray) json.get("graphData");
-		GraphDataObj[] graphDataArray = JSONFormatter.returnGraphDataArray(graphData_fromJSON);
-		
-		TreeMap<Integer, Integer> colorMap = new TreeMap<Integer, Integer>();
-		TreeSet<Integer> usedColorIndexes = new TreeSet<Integer>();
-		JSONArray vms_object = JSONFormatter.assignFragmentsToVMS(fragments_fromJSON, vms_fromJSON, colorMap, usedColorIndexes, graphDataArray);
-		JSONArray f = JSONFormatter.returnFragmentArray(graphDataArray, fragments_fromJSON, colorMap, usedColorIndexes);
-		JSONArray transitionInfo = new JSONArray();
-//		transitionInfo = JSONFormatter.returnedTransitionArray(nash, vms_fromJSON, null, fragments_fromJSON);
-		JSONObject seriesUnit = new JSONObject();
+		Collection<Collection<Fragment>> currentConfiguration = null;
 
-		seriesUnit.put("tuples", json.get("graphData"));
-		seriesUnit.put("fragments", f);
-		seriesUnit.put("vms", vms_object);
-		seriesUnit.put("transition", null);
-		seriesArray.add(seriesUnit);
-		JSONObject returnedJSON = new JSONObject();
-		returnedJSON.put("maxFragmentsInVms", MAX_FRAGS_IN_VM);
-		returnedJSON.put("series", seriesArray);
-		return returnedJSON.toJSONString();
+		JsonObject dataForTimestamp = new JsonObject();
+		// add the data for the tuple value graph to the JSON output
+		dataForTimestamp.add("graph values", 
+				nash.getEstimatedTupleValues().stream()
+				.map(rav -> rav.toJSON())
+				.collect(new JsonArrayCollector()));
+		
+		
+		// time to do a transition. compute new fragments
+		List<Fragment> fragments = new ArrayList<>(nash
+				.doFragmentation(FragmentationStrategy.OPTIMAL, NUM_FRAGMENTS)
+				.getFragments());
+		Collections.sort(fragments);
+		   
+		// add the fragment data to the JSON output
+		dataForTimestamp.add("fragments",
+				fragments.stream()
+				.map(f -> f.toJSON(nash))
+				.collect(new JsonArrayCollector()));
+
+		// allocate the fragments onto VMs and build a JSON structure mapping each VM to
+		// a fragment index.
+		Collection<Collection<Fragment>> vms = nash.doFragmentAllocation(fragments);
+		currentConfiguration = vms;
+		JsonArray jsonVMs = vms.stream()
+				.map(vm -> RenderExample.fragmentsToIndexes(vm, fragments))
+				.map(idxs -> idxs.stream().map(i -> Json.value(i)).collect(new JsonArrayCollector()))
+				.collect(new JsonArrayCollector());
+		dataForTimestamp.add("vms", jsonVMs);
+
+		
+		// compute the profit for each fragment
+		SortedMap<Fragment, Double> profits = new TreeMap<>(
+				nash.calculateFragmentProfits(currentConfiguration));
+		JsonArray jsonProfits = new JsonArray();
+		profits.values().forEach(jsonProfits::add);
+		dataForTimestamp.add("fragment profits", jsonProfits);
+		
+		// compute the ideal deltas for each fragment
+		SortedMap<Fragment, Double> deltas = new TreeMap<>(
+				nash.calculateIdealDeltas(currentConfiguration));
+		JsonArray jsonDeltas = new JsonArray();
+		deltas.values().forEach(jsonDeltas::add);
+		dataForTimestamp.add("fragment deltas", jsonDeltas);
+		
+		output.add(dataForTimestamp);
+		
+		JsonObject wrapper = new JsonObject();
+		wrapper.add("data", output);
+		System.out.println(wrapper.toString());
+		return wrapper.toString();
 	}
-	
 }
+	
+
